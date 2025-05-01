3d7e2e76ed8408ff9b22371dc72014f11b09625c
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2025-05-01 15:26:49 +0300

    some changes -- organization

--- Makefile
+++ Makefile
@@ -6,6 +6,22 @@ WORLD=our_runway.sdf
 MODEL=gazebo-iris
 #MODEL=iris_with_gimbal
 
+
+# Define the variable name and value
+GZ_SIM_SYSTEM_PLUGIN_PATH := $$HOME/Desktop/code/matek/src/ardupilot_gazebo/build:$$GZ_SIM_SYSTEM_PLUGIN_PATH
+GZ_SIM_RESOURCE_PATH := $$HOME/Desktop/code/matek/src/ardupilot_gazebo/models:$$HOME/Desktop/code/matek/src/ardupilot_gazebo/worlds:$$GZ_SIM_RESOURCE_PATH
+
+define set_env_var_fn
+	@if [ -z "$$$(grep -E "^export $(1)=" ~/.bashrc)" ]; then \
+		echo "$(1) is not set in .bashrc."; \
+		echo "Adding $(1) to .bashrc..."; \
+		echo "export $(1)=\"$(2)\"" >> ~/.bashrc; \
+		echo "$(1) added to .bashrc."; \
+	else \
+		echo "$(1) is already defined in .bashrc."; \
+	fi
+endef
+
 gz:
 	gz sim -v4 -r ${WORLD} 
 
@@ -17,3 +33,9 @@ create:
 
 camera_feed:
 	gst-launch-1.0 -v udpsrc port=5600 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! avdec_h264 ! videoconvert ! jpegenc ! multipartmux ! tcpserversink host=0.0.0.0 port=8080
+
+
+# Check if the variable is defined and add it to .bashrc if it's not
+set_env_vars:
+	$(call set_env_var_fn,GZ_SIM_SYSTEM_PLUGIN_PATH,$(GZ_SIM_SYSTEM_PLUGIN_PATH))
+	$(call set_env_var_fn,GZ_SIM_RESOURCE_PATH,$(GZ_SIM_RESOURCE_PATH))
---
+++ run_sim.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Usage info
+usage() {
+  echo "Usage: $0 [-v] [-w <world_file.sdf>]"
+  echo "  -v               scoop de pop scoop le poop de poop"
+  echo "  -w <file.sdf>    Specify world file to load"
+  echo "Example:"
+  echo "  $0 -v -w iris_runway.sdf"
+  exit 1
+}
+# Commands
+GAZEBO_CMD="gz sim -v4 -r delivery_runway.sdf"
+
+# Default: no verbose flag
+VERBOSE=0
+WORLD_FILE="delivery_runway.sdf"
+
+# Parse CLI options
+while getopts ":vw:" opt; do
+  case $opt in
+  v) VERBOSE=1 ;;
+  w) WORLD_FILE="$OPTARG" ;;
+  *) usage ;;
+  esac
+done
+
+# Commands
+GAZEBO_CMD="gz sim -v4 -r delivery_runway.sdf"
+ARDU_CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console"
+
+# Name of the tmux session
+SESSION="gz_ardupilot"
+
+# Build the command
+GAZEBO_CMD="gz sim -v4 -r"
+GAZEBO_CMD="$GAZEBO_CMD $WORLD_FILE"
+
+[ "$VERBOSE" -eq 1 ] && echo "scoop de pop scoop le poop de poop"
+
+# Cleanup function
+cleanup() {
+  echo "Cleaning up temporary files..."
+  # Add file cleanup commands here, for example:
+  rm mav.tlog* mav.tlog.raw mav.parm eeprom.bin
+  run -rf terrain/
+  echo "Cleanup done."
+}
+
+# Set the cleanup function to run on exit
+trap cleanup EXIT
+
+# Kill old session if exists
+tmux has-session -t $SESSION 2>/dev/null
+if [ $? -eq 0 ]; then
+  tmux kill-session -t $SESSION
+fi
+
+# Start session and name the window
+tmux new-session -d -s $SESSION -n gazebo "$GAZEBO_CMD"
+tmux split-window -h -t $SESSION "$ARDU_CMD"
+
+# Rename second pane's window (if ArduPilot tries to rename)
+tmux select-pane -t $SESSION:.1
+tmux select-window -t $SESSION:0
+#tmux rename-window -t $SESSION:0 'Gazebo+ArduPilot'
+
+# Layout and attach
+tmux select-layout -t $SESSION tiled
+tmux attach-session -t $SESSION

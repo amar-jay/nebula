75bb8cc7964a5b77dbc9861227620c94f03a5ead
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2025-10-09 13:14:48 +0300

    messaging docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MATEK Nebula'
+copyright = '2025, Apache'
+author = 'amar-jay'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0.0'
 
 # -- General configuration
 
